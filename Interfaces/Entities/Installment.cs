775980d7ec0d74e0add5c8bb01545e825a7957ce
Guilherme <guilhermetorrente@outlook.com>
2023-05-25 05:28:26 -0300

    Aula de Interfaces

---
+++ Interfaces/Entities/Installment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Interfaces.Entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = DueDate;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return DueDate.ToString("dd/MM/yyyy") + " - " + Amount.ToString("F2", CultureInfo.InvariantCulture); 
+        }
+    }
+}
---
+++ Interfaces/Services/ContractService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Interfaces.Entities;
+
+namespace Interfaces.Services
+{
+    class ContractService
+    {
+        private IOnlinePaymentService _onlinePaymentService;
+
+        public ContractService(IOnlinePaymentService onlinePaymentService)
+        {
+            _onlinePaymentService = onlinePaymentService;
+        }
+
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basicQota = contract.TotalValue / months;
+            for (int i = 1; i <= months; i++)
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updatedQota = basicQota + _onlinePaymentService.Interest(basicQota, i);
+                double fullQota = updatedQota + _onlinePaymentService.PaymentFee(updatedQota);
+                contract.AddInstallment(new Installment(date, fullQota));
+            }
+        }
+    }
+}
